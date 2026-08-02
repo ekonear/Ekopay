fb325dce465f733088684a76576c3d51d2631dc1
ekonear <ekonurh121@gmail.com>
2026-08-02 14:58:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// GANTI baris di bawah ini dengan Database URL dari Firebase Realtime Database kamu.
+// Contoh: "https://kas-meja-default-rtdb.asia-southeast1.firebasedatabase.app"
+// Lihat README.md untuk cara mendapatkannya (gratis, ±3 menit).
+const FIREBASE_URL = "https://eko-pay-d0f3c-default-rtdb.asia-southeast1.firebasedatabase.app";
