f097530a47034bab019a8da05c047d659a8d8fd7
ekonear <ekonurh121@gmail.com>
2026-08-16 20:02:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Lihat README.md untuk cara mendapatkannya (gratis, tanpa kartu kredit).
 
 // 1. Database URL — dari Realtime Database (bagian atas halaman setelah dibuat)
-const FIREBASE_URL = "https://console.firebase.google.com/project/eko-pay-d0f3c/database/eko-pay-d0f3c-default-rtdb/data";
+const FIREBASE_URL = "https://eko-pay-d0f3c-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
 // 2. Web API Key — dari Project Settings (⚙️) → General → Web API Key
 const FIREBASE_API_KEY = "AIzaSyBCZrf9ruEy0lgorINOlfHchv62C4Etsgk";
